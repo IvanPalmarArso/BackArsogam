e7764a1a6826e0b0a00390ab8a8b8fdd56153ab3
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-11-22 23:06:44 -0500

    New Origin

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const app = express();
 //Cors
 app.use(
     cors({
-        origin : 'http://localhost:5173',
+        origin : ['http://localhost:5173','https://arsogam.netlify.app'],
         credentials : true
     })
 )
