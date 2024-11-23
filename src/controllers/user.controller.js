4d34cb9f5ec7dca931873122abaa8278ad6a89ff
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-11-22 23:53:55 -0500

    Partitioned Attributes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,7 +54,7 @@ export const registerUser = async(req, res) => {
             isAdmin : saveUser.isAdmin
         })
 
-        res.cookie('token', token, {sameSite : 'none', secure : true, httpOnly : true})
+        res.cookie('token', token, {sameSite : 'none', secure : true, httpOnly : true, Partitioned : true})
 
         res.json(
             saveUser
@@ -94,7 +94,7 @@ export const loginUser = async(req, res) => {
         })
 
         res.cookie('token', token, {
-            sameSite : 'none', secure : true, httpOnly : true
+            sameSite : 'none', secure : true, httpOnly : true, Partitioned : true
         })
 
         res.json({
