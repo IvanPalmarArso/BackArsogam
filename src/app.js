f517691810e928be6981ba7565f785f408a6e170
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-12-02 09:44:28 -0500

    Custom Domain

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 //Cors
 app.use(cors({
-    origin : ['https://arsogam.netlify.app','http://localhost:5173','https://backarsogam-production.up.railway.app'],
+    origin : ['https://arsogam.com','https://arsogam.netlify.app','http://localhost:5173','https://backarsogam-production.up.railway.app'],
     credentials : true
 }))
 //Morgan
